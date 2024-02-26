66761e95247255479ae3318c0a9e832229f8defd
rabiebn <rabie.bnmr@gmail.com>
2024-02-26 16:16:42 +0100

    6: added save after deleting a state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -32,6 +32,7 @@ def delete_state(state_id):
     """Docs"""
     try:
         storage.delete(storage.get('State', state_id))
+        storage.save()
         return jsonify({}), 200
     except Exception as e:
         abort(404)
