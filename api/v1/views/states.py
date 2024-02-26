1ff3f3ff2a8637c9e68898af7e50ce6c70787b25
rabiebn <rabie.bnmr@gmail.com>
2024-02-26 16:45:23 +0100

    6: added PUT method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,7 +20,6 @@ def get_all_states():
 def get_state(state_id):
     """Docs"""
     try:
-        state = storage.get('State', state_id)
         return jsonify(storage.get('State', state_id).to_dict())
     except Exception:
         abort(404)
@@ -41,11 +40,27 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Docs"""
-    if not request.json:
+    if not request.get_json():
         return jsonify({"error": "Not a JSON"}), 400
-    if 'name' not in request.json:
+    if 'name' not in request.get_json():
         return jsonify({"error": "Missing name"}), 400
 
     new_state = State(**request.get_json())
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Docs"""
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    for key, val in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, val)
+    state.save()
+
+    return jsonify(state.to_dict()), 200
